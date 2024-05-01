8e54e0fcba93809e0fa8b007bb6a094119a48c8d
Saqib Qamar <dr.saqibqamar@gmail.com>
2024-05-01 16:38:51 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ import random
 from datetime import datetime
 import shutil
 from flask import current_app
+from fil_finder import FilFinder2D
+import astropy.units as u
 
 #from segment import measure
 
@@ -29,30 +31,17 @@ app = Flask(__name__)
 def detect(model, img, imgsz):
         # Get img shape
    # height, width, channels = img.shape
-    results = model.predict(source=img.copy(), project="static/Result", name="prediction", imgsz=imgsz, save=True, iou=0.8, conf=0.6, save_txt=False)
+    results = model.predict(source=img.copy(), project="Result", name="pred", overlap_mask=False, imgsz=imgsz, save=True, iou=0.8, conf=0.6, save_txt=False)
     result = results[0]
-    segments = []
-    total_area= []
-    h2, w2, c2 = results[0].orig_img.shape
-    for x in result.masks.cpu().data.numpy().astype('uint8'):
-        x = cv2.resize(x, (w2, h2))
-        c = cv2.findContours(x, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
-        contour_area = 0
-        for contour in c:
-            area = cv2.contourArea(contour)
-            contour_area += area
-        c = np.concatenate([x.reshape(-1, 2) for x in c])
-        segments.append(c.astype('float32'))
-        total_area.append(contour_area)
         
-
+    # Extract Masks, bounding boxes, class IDs, and scores from the result
+    segment = result.masks.cpu().data.numpy()
     bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
-        # Get class ids
     class_ids = np.array(result.boxes.cls.cpu(), dtype="int")
-        # Get scores
     scores = np.array(result.boxes.conf.cpu(), dtype="float").round(2)
-    #print(class_ids)
-    return bboxes, class_ids, segments, scores, total_area
+    
+    # Return the detected bounding boxes, class IDs, segments, and scores
+    return bboxes, class_ids, segment, scores
 
 def draw_mask(img, pts, color, alpha=0.5):
     h, w, _ = img.shape
@@ -80,7 +69,7 @@ def random_colors(N, bright=True):
 class_names = ['Fibre', 'Vessel']
 colors = random_colors(len(class_names))
 
-model = YOLO("model/best.pt")
+model = YOLO("C:/Users/drsaq/Downloads/best.pt")
 
 def measure(model, path):
     img = cv2.imread(path)
@@ -97,28 +86,75 @@ def measure(model, path):
         imgsz = 2048  
         
     list2=[]
-    bboxes, classes, segmentations, scores, area = detect(model, img, imgsz)
-    for bbox, class_id, seg, score, area in zip(bboxes, classes, segmentations, scores, area):
+    bboxes, classes, segmentations, scores = detect(model, img, imgsz)
+    #bboxes, classes, segmentations, scores, area = detect(model, img, imgsz)
+    for i, (bbox, class_id, seg, score) in enumerate(zip(bboxes, classes, segmentations, scores)):
     # print("bbox:", bbox, "class id:", class_id, "seg:", seg, "score:", score)
-        (x, y, x2, y2) = bbox
-        rect = cv2.minAreaRect(seg)
-        box1 = cv2.boxPoints(rect)
-        box1 = np.int0(box1)
-    #print(box1)
-        a,b,c,d=box1
-        dst1= ((a[0] - b[0])**2 + (a[1] - b[1])**2)**0.5
-        dst2 = ((b[0] - c[0])**2 + (b[1] - c[1])**2)**0.5
-        dst1 = dst1 * 0.65
-        dst2 = dst2 * 0.65
-        if dst1 > dst2:
-            Length = dst1
-        else:
-            Length = dst2
-    #area_px = cv2.contourArea(seg)
-        area_px = area * 0.65
-        Width = round(area_px/Length, 2)
-        list1 = class_names[class_id], Length, Width, area_px
+        color = colors[i]
+        (x, y, x2, y2) = bbox 
+        h, w = seg.shape
+        mask_3channel = cv2.merge((seg, seg, seg))
+        # Get the size of the original image (height, width, channels)
+        h2, w2, c2 = img.shape
+        # Resize the mask to the same size as the image
+        x = cv2.resize(seg, (w2, h2)).astype('uint8')
+        # Find contours in the mask
+        d = cv2.findContours(x, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        if d:    
+            cntsSorted = sorted(d, key=lambda x: cv2.contourArea(x) , reverse =  True)
+            
+        if d: 
+            largest_contour = cntsSorted[0] 
+        mask = cv2.resize(mask_3channel, (w2, h2)).astype(int)
+
+        ### Length Calculation 
+
+        # Create a blank image with the same dimensions as the mask, but with 3 channels (RGB)
+        drawing = np.zeros((mask.shape[0], mask.shape[1], 3))
+        # Draw the largest contour from the mask onto the drawing image and fill it with green color
+        cv2.drawContours(drawing, [cntsSorted[0]] , -1 , color = (0,255,0) , thickness = cv2.FILLED)
+        drawing = drawing.astype(np.uint8)
+        drawing = cv2.cvtColor(drawing, cv2.COLOR_BGR2GRAY)
+        # Apply the Zhang-Suen thinning algorithm to the grayscale drawing to obtain a skeleton
+        thinned = cv2.ximgproc.thinning(drawing, thinningType = cv2.ximgproc.THINNING_ZHANGSUEN)
+        
+        skeleton = thinned
+        
+        # Initialize an instance of the FilFinder2D class with the skeleton, a distance parameter, and the skeleton as the mask
+        fil = FilFinder2D(skeleton, distance=250 * u.pc, mask=skeleton)
+        # Preprocess the image or skeleton by flattening the intensity values
+        fil.preprocess_image(flatten_percent=85)
+        fil.create_mask(border_masking=True, verbose=False,
+        use_existing_mask=True)
+        fil.medskel(verbose=False)
+        fil.analyze_skeletons(branch_thresh=40* u.pix, skel_thresh=10 * u.pix, prune_criteria='length')
+        # Assign the longest path or skeleton from the FilFinder2D instance to the 'mask' variable
+        mask = fil.skeleton_longpath
+        Length = np.sum(mask)
+
+        #print(f' Object Length is: {length:.2f}')
+        
+        ### Width Calculation 
+
+        # Compute the distance transform
+        dist_transform = cv2.distanceTransform(drawing, cv2.DIST_L2, 5)
+
+        # Find the maximum value in the distance transform
+        max_dist = np.max(dist_transform)
+
+        # The maximum value corresponds to the thickness of the thickest part of the fiber
+        fiber_thickness = max_dist * 2
+        Width = round(max_dist * 2, 2)
+
+        #print(f"Object Width is: {fiber_thickness:.2f} pixels")
+        
+        Area = cv2.contourArea(cntsSorted[0])
+        list1 = class_names[class_id], Length, Width, Area
         list2.append(list1)
+        
+        cnt = cntsSorted[0].reshape(-1, 2)
+
+        img = draw_mask(img, [cnt], color)
     
     #cv2.drawContours(img, [box1], 0, (0, 0, 255), 2)
         #img = draw_mask(img, [seg], colors[class_id])
